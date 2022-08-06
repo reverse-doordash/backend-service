9eb7731cb8d19ccfb661b5e9317983f8b1e9d6f3
Kavi <kavipatel04@gmail.com>
2022-08-06 08:33:42 -0700

    generating random coordinates testing

--- index.js
+++ index.js
@@ -23,6 +23,11 @@ var driver_latitude = 34.0;
 var client_longitude = 34.0;
 var client_latitude = 34.0;
 
+function generateRandCoords(){
+    let randDigits = parseInt(Math.random() * Math.pow(10, 11));
+    console.log(randDigits);
+}
+
 //to get a json with locations use
 app.get('/getlocs', function (req, res) {
   let date_obj = new Date();
@@ -61,6 +66,45 @@ app.post('/clientlocupdate', function (request, response) {
   }
 });
 
+  app.post('/clientlocupdate', function(request, response) {
+    console.log('POST /')
+    
+    if (request.headers.passwd == process.env.SECURE_HEADER_PASSCODE){
+        client_latitude = request.body.client_latitude;
+        client_longitude = request.body.client_longitude;
+        response.writeHead(200, {'Content-Type': 'text/html'})
+        response.end('thanks, received')
+    } else {
+        response.writeHead(200, {'Content-Type': 'text/html'})
+        response.end('unable to update, check passcode')
+    }
+    
+    
+    
+  })
+
+  app.post('/discoMode', function(request, response) {
+    console.log('POST /')
+    
+    if (request.headers.passwd == process.env.SECURE_HEADER_PASSCODE){
+        if (request.params.discoMode == true){
+
+        }
+        client_latitude = request.body.client_latitude;
+        client_longitude = request.body.client_longitude;
+        response.writeHead(200, {'Content-Type': 'text/html'})
+        response.end('thanks, received')
+    } else {
+        response.writeHead(200, {'Content-Type': 'text/html'})
+        response.end('unable to update, check passcode')
+    }
+    
+    
+    
+  })
+
+  generateRandCoords();
+  
 // Setting the server to listen at port 3000
 app.listen(3000, function (req, res) {
   console.log('Server is running at port 3000');
